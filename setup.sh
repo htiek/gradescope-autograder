404b156c37c596fdce0bfdad26ac14f7572bf6db
Keith Schwarz <htiek@cs.stanford.edu>
2018-08-01 14:36:31 -0700

    It works!

--- setup.sh
+++ setup.sh
@@ -1,9 +1,7 @@
 # Install dependencies.
 add-apt-repository ppa:ubuntu-toolchain-r/test -y   &&
 apt-get update                                      &&
-apt-get upgrade -y                                  &&
-apt-get install g++-6 -y                            &&
+apt-get install build-essential g++-6 -y
 
-# Move everything from the source/ directory up a level. That extra indirection is just a
-# wee bit confusing. :-)
-mv source/* .
+# Move everything into the same directory arrangment as in the local version
+mv autograder/source/* autograder/
